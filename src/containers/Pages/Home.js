9866766f46b01ab214f32b1855bb3e273df296e1
hardchopshoppirate <hardchopshoppirate@gmail.com>
2016-12-14 19:49:03 +0200

    Home page

--- src/containers/Pages/Home.js
+++ src/containers/Pages/Home.js
@@ -18,53 +18,6 @@ export default class Home extends Component {
                     <Icon name="home" type="fa" size={18}/>
                     <span>Home</span>
 
-                    <div styleName="row">
-                        <div styleName="col-xs">
-                            Image example via import from lib (but in lib use 'require'):
-                            <code>
-                                -- lib/index.js
-                                <br />
-                                <br />
-                                const Avatar = require('./images/example/users/avatar-3.jpg');
-                                <br />
-                                <br />
-                                -- this file
-                                <br />
-                                <br />
-                                import {Avatar} from '../../lib';
-                                <br />
-                                <br />
-                                ...
-                                <br />
-                                <br />
-                                set Image into src attribute as an object
-                            </code>
-                            <div>
-                                <img src={Avatar} alt=""/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div styleName="row">
-                        <div styleName="col-xs">
-                            Image example via 'require':
-                            <code>
-                                -- this file
-                                <br />
-                                <br />
-                                const Image = require('../../lib/images/example/gallery/4.jpg');
-                                <br />
-                                <br />
-                                ...
-                                <br />
-                                <br />
-                                set Image into src attribute as an object
-                            </code>
-                            <div>
-                                <img src={Image} alt=""/>
-                            </div>
-                        </div>
-                    </div>
                 </h2>
             </div>
         )
